da355289db1bc958fbbe91e07bde2aa3218041db
ali <aliakbar.dandach@isae.edu.lb>
2022-01-23 22:48:49 +0200

    basic authentication entry point

--- src/main/java/com/ccc/hrapp/common/configuration/SecurityConfiguration.java
+++ src/main/java/com/ccc/hrapp/common/configuration/SecurityConfiguration.java
@@ -1,42 +1,41 @@
 package com.ccc.hrapp.common.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	private final AuthenticationEntryPoint authEntryPoint;
 
-	@Value("ba.username")
+	@Value("${ba.username}")
 	private String username;
 
-	@Value("ba.password")
+	@Value("${ba.password}")
 	private String password;
 
-
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("admin").password(passwordEncoder().encode("admin")).roles("ADMIN");
+	public SecurityConfiguration(AuthenticationEntryPoint authEntryPoint) {
+		this.authEntryPoint = authEntryPoint;
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-				.anyRequest()
-				.authenticated()
-				.and()
-				.httpBasic();
+		http.csrf().disable().authorizeRequests()
+				.anyRequest().authenticated()
+				.and().httpBasic()
+				.authenticationEntryPoint(authEntryPoint);
 	}
 
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser(username)
+				.password(authEntryPoint.passwordEncoder().encode(password))
+				.roles("USER");
 	}
+
 }
